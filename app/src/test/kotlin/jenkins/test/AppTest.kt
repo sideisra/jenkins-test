437b153ff53f480ca7502cf9820bfdfee4643f06
sideisra <denny.israel@ottogroup.com>
2023-02-28 10:08:54 +0100

    revert test failure

--- app/src/test/kotlin/jenkins/test/AppTest.kt
+++ app/src/test/kotlin/jenkins/test/AppTest.kt
@@ -10,6 +10,6 @@ import kotlin.test.assertNotNull
 class AppTest {
     @Test fun appHasAGreeting() {
         val classUnderTest = App()
-        assertThat(classUnderTest.greeting).isEqualTo("Hello Worl")
+        assertThat(classUnderTest.greeting).isEqualTo("Hello World")
     }
 }
